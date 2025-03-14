d4580ab870734b10218d0a801829b07f69ccb456
Affe O <alfrokot100@student.kau.se>
2025-03-14 10:53:19 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home"
+import About from "./Components/About"
+import Contact from "./Components/Contact"
+import Cv from "./Components/Cv"
+import Portfolio from "./Components/Projects"
+import './App.css'
+
+function App() {
+
+  return (
+    <Router>
+            <Header /> {/* Headern syns på alla sidor */}
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/cv" element={<Cv />} />
+          <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+
+    
+  );
+}
+
+export default App
